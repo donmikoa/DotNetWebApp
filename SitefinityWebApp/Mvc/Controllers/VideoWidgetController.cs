391aea9b8f47d56d3250ce0c614b0ebacf5ded6b
Michael Iyaomolere <michael.iyaomolere@snhu.edu>
2020-04-14 13:24:05 -0400

    Implementing the VideoWidget Model

---
+++ SitefinityWebApp/Mvc/Controllers/VideoWidgetController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using SitefinityWebApp.Mvc.Models;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
+
+namespace SitefinityWebApp.Mvc.Controllers
+{
+    public class VideoWidgetController : Controller
+    {
+        [Category("String Properties")]
+        [DisplayName("First Name")]
+        [Description("Enter the First Name")]
+        public string Message { get; set; }
+        // GET: VideoWidget
+        public ActionResult Index()
+        {
+            var model = new VideoWidgetModel();
+
+            if (string.IsNullOrEmpty(this.Message))
+            { model.Message = "Hello World"; }
+            else { model.Message = this.Message; }
+            return View("Default", model);
+        }
+    }
+}
---
+++ SitefinityWebApp/Mvc/Models/VideoWidgetModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using SitefinityWebApp.Mvc.Models;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
+
+namespace SitefinityWebApp.Mvc.Models
+{
+    public class VideoWidgetModel
+    {
+        public string Message { get; set; }
+    }
+}
