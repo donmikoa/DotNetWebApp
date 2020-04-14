7a8a169b38f3bd73d13dd09baa0273d02b9eb3a4
Michael Iyaomolere <michael.iyaomolere@snhu.edu>
2020-04-14 12:59:20 -0400

    Creating the Video Library Widget

---
+++ SitefinityWebApp/Mvc/Controllers/MessageWidgetController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using SitefinityWebApp.Mvc.Models;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
+
+namespace SitefinityWebApp.Mvc.Controllers
+{
+    [ControllerToolboxItem(Name = "MessageWidget", Title = "Message Widget", SectionName = "MvcWidgets")]
+    public class MessageWidgetController : Controller
+    {
+        [Category("String Properties")]
+        [DisplayName("First Name")]
+        [Description("Enter the First Name")]
+        public string Message { get; set; }
+        // GET: MessageWidget
+        public ActionResult Index()
+        {
+            var model = new MessageWidgetModel();
+
+            if (string.IsNullOrEmpty(this.Message))
+            { model.Message = "Hello World"; }
+            else { model.Message = this.Message; }
+            return View("Default", model);
+        }
+    }
+}
