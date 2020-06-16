c2938d9becf7051187a0a56579a0df353b5c652c
Michael Iyaomolere <michael.iyaomolere@snhu.edu>
2020-06-16 10:40:42 -0400

    Implementing accredited filter on the A-Z Charity List

--- SitefinityWebApp/Mvc/Models/AllCharity.cs
+++ SitefinityWebApp/Mvc/Models/AllCharity.cs
@@ -10,19 +10,26 @@ namespace SitefinityWebApp.Mvc.Models
     public class AllCharity : IComparable<AllCharity>
     {
 
-        public AllCharity(string title, string reporturl)
+        public AllCharity(string title, string reporturl, bool accredited)
         {
             this._name = title;
             this._url = reporturl;
+            this._accreditied = accredited;
 
         }
 
         public string Name { get { return this._name; } }
         public string Url { get { return this._url; } }
 
+        public bool Accredited
+        {
+            get { return this._accreditied; }
+        }
+
 
         private readonly string _name;
         private readonly string _url;
+        private readonly bool _accreditied;
 
         public int CompareTo(AllCharity other)
         {
